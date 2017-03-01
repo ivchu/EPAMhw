7cd4eee6e87e899f3eeee023026cb30044f8675a
Ivan Chuvakhin <chuvakhinivan@gmail.com>
2017-03-01 23:15:21 +0300

    AtomicSubmarine can go

---
+++ unit02/tasks06_07/src/main/java/hw/epam/submarine/AtomicSubmarine.java
@@ -0,0 +1,12 @@
+package hw.epam.submarine;
+
+/**
+ * Created by MM8 on 01.03.2017.
+ */
+public class AtomicSubmarine {
+    public static class EngineForAtomicSubmarine{
+        public int startEngine(){
+            return 1;
+        }
+    }
+}
