64251385ff9b0d00f4bcf9ae5f1572322c70cde1
Ivan Chuvakhin <chuvakhinivan@gmail.com>
2017-03-02 00:15:27 +0300

    Added annotations for submarine

--- unit02/tasks06_07/src/main/java/hw/epam/submarine/AtomicSubmarine.java
+++ unit02/tasks06_07/src/main/java/hw/epam/submarine/AtomicSubmarine.java
@@ -3,9 +3,12 @@ package hw.epam.submarine;
 /**
  * Created by MM8 on 01.03.2017.
  */
+@SubmarineAnno(name = "ivchu",
+        date = "01.03.2017",
+        ver = "1.0")
 public class AtomicSubmarine {
-    public static class EngineForAtomicSubmarine{
-        public int startEngine(){
+    public static class EngineForAtomicSubmarine {
+        public int startEngine() {
             return 1;
         }
     }
---
+++ unit02/tasks06_07/src/main/java/hw/epam/submarine/SubmarineAnno.java
@@ -0,0 +1,11 @@
+package hw.epam.submarine;
+
+
+/**
+ * Created by MM8 on 02.03.2017.
+ */
+public @interface SubmarineAnno {
+    String name();
+    String date();
+    String ver();
+}
