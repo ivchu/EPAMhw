65bbf34c71bb869cdea3774d32e6fe43fd572a7e
Ivan Chuvakhin <chuvakhinivan@gmail.com>
2017-03-06 18:17:03 +0300

    updated peogram and tested on some props

--- unit03/task02/src/main/java/hw/epam/locale/QATaskLocaleAndResourceBundle.java
+++ unit03/task02/src/main/java/hw/epam/locale/QATaskLocaleAndResourceBundle.java
@@ -12,36 +12,46 @@ public class QATaskLocaleAndResourceBundle {
     private ResourceBundle answers;
 
     public static void main(String[] args) {
-
+        new QATaskLocaleAndResourceBundle().findAnswers();
     }
 
     public void findAnswers() {
         Scanner userInputReader = new Scanner(System.in);
-        String userInput = userInputReader.nextLine();
         List<String> keys = new ArrayList<>();
         while (true) {
             System.out.println("Choose lenguage, ru - Russian, en - English\nfor exit use q");
+            String userInput = userInputReader.nextLine();
             if (userInput.equals("ru")) {
-                questions = ResourceBundle.getBundle("Questions_ru_RU", localeRU);
-                keys.addAll(questions.keySet());
-                System.out.println("Напишите номер вопроса на который хотите найти ответ");
-                String answerNumber = userInputReader.nextLine();
-                if (keys.contains(answerNumber)) {
-                    answers = ResourceBundle.getBundle("Answers_ru_RU", localeRU);
-                    System.out.println(answers.getString(answerNumber));
-                } else {
-                    System.out.println("Такого номера вопроса нет, попробуйте снова");
+                while (true) {
+                    questions = ResourceBundle.getBundle("Questions_ru_RU", localeRU);
+                    keys.addAll(questions.keySet());
+                    System.out.println("Напишите номер вопроса на который хотите найти ответ");
+                    String answerNumber = userInputReader.nextLine();
+                    if (keys.contains(answerNumber)) {
+                        answers = ResourceBundle.getBundle("Answers_ru_RU", localeRU);
+                        System.out.println(answers.getString(answerNumber));
+                        break;
+                    } else if (answerNumber.equals("q")) {
+                        break;
+                    } else {
+                        System.out.println("Такого номера вопроса нет, попробуйте снова");
+                    }
                 }
             } else if (userInput.equals("en")) {
-                questions = ResourceBundle.getBundle("Questions_en_US", localeEN);
-                keys.addAll(questions.keySet());
-                System.out.println("Choose question, please");
-                String answerNumber = userInputReader.nextLine();
-                if (keys.contains(answerNumber)) {
-                    answers = ResourceBundle.getBundle("Answers_en_US", localeRU);
-                    System.out.println(answers.getString(answerNumber));
-                } else {
-                    System.out.println("We don`t have such question, pls try again");
+                while (true) {
+                    questions = ResourceBundle.getBundle("Questions_en_US", localeEN);
+                    keys.addAll(questions.keySet());
+                    System.out.println("Choose question, please");
+                    String answerNumber = userInputReader.nextLine();
+                    if (keys.contains(answerNumber)) {
+                        answers = ResourceBundle.getBundle("Answers_en_US", localeRU);
+                        System.out.println(answers.getString(answerNumber));
+                        break;
+                    } else if (answerNumber.equals("q")) {
+                        break;
+                    } else {
+                        System.out.println("We don`t have such question, pls try again");
+                    }
                 }
             } else if (userInput.equals("q")) {
                 break;
---
+++ unit03/task02/src/test/java/hw/epam/locale/QATaskLocaleAndResourceBundleTest.java
@@ -0,0 +1,11 @@
+package hw.epam.locale;
+
+/**
+ * Created by MM8 on 06.03.2017.
+ */
+public class QATaskLocaleAndResourceBundleTest {
+    public void testForQA(){
+
+    }
+
+}
