d4c43ee8fc20f0b16452267d4d9cc1c7f0a33cbb
Ivan Chuvakhin <chuvakhinivan@gmail.com>
2017-03-10 14:48:55 +0300

    reading from file

---
+++ unit04/task01/src/test/java/hw/epam/bitejava/KeyWordsTest.java
@@ -0,0 +1,15 @@
+package hw.epam.bitejava;
+
+import org.junit.Test;
+
+/**
+ * Created by MM8 on 10.03.2017.
+ */
+public class KeyWordsTest {
+    KeyWords testingUnit;
+    @Test
+    public void loadInfoFromFile() throws Exception {
+
+    }
+
+}
