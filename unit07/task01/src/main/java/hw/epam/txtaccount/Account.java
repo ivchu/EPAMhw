f555f3306fc7d08eb5d58f3913e5a60b196b39d4
Ivan Chuvakhin <chuvakhinivan@gmail.com>
2017-03-15 21:26:09 +0300

    Account class some structure

--- unit07/task01/src/main/java/hw/epam/txtaccount/Account.java
+++ unit07/task01/src/main/java/hw/epam/txtaccount/Account.java
@@ -2,5 +2,35 @@ package hw.epam.txtaccount;
 
 
 public class Account {
+    private int accountId;
+    private int amountOfMoney;
 
+    Account(int accountId, int amountOfMoney) {
+        this.accountId = accountId;
+        this.amountOfMoney = amountOfMoney;
+    }
+
+    void readInfoFromFile(){
+
+    }
+
+    void writeInfoToFile(){
+
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    private void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getAmountOfMoney() {
+        return amountOfMoney;
+    }
+
+    void setAmountOfMoney(int amountOfMoney) {
+        this.amountOfMoney = amountOfMoney;
+    }
 }
