b3828c61546bc0e2f8dafb6c93d55736f018ba3a
Ivan Chuvakhin <chuvakhinivan@gmail.com>
2017-03-15 21:32:37 +0300

    some thoughts about bank

--- unit07/task01/src/main/java/hw/epam/txtaccount/Bank.java
+++ unit07/task01/src/main/java/hw/epam/txtaccount/Bank.java
@@ -1,5 +1,22 @@
 package hw.epam.txtaccount;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
+    private volatile int idForCreation = 0;
+    private Map<Integer, Account> accounts = new HashMap<>();
+
+    public void transferMoney(Integer from, Integer to){
+
+    }
+
+    public void depositMoney(Integer to){
+
+    }
+
+    public void createAccount(){
+        
+    }
 }
