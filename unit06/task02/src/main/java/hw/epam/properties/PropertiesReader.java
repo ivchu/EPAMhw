531bfa4a2fb5da75fcf4c1c2389090be497ef21f
Ivan Chuvakhin <chuvakhinivan@gmail.com>
2017-03-27 20:19:12 +0300

    + method which readinf properties in map

--- unit06/task02/src/main/java/hw/epam/properties/PropertiesReader.java
+++ unit06/task02/src/main/java/hw/epam/properties/PropertiesReader.java
@@ -1,7 +1,16 @@
 package hw.epam.properties;
 
 
+import hw.epam.properties.Exceptions.NoSuchPropertyFileException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 public class PropertiesReader {
     private Map<String, String> properties;
@@ -9,14 +18,49 @@ public class PropertiesReader {
     public PropertiesReader() {
     }
 
-    public Map<String, String> readPropertiesInMap(String path){
-        if (path == null){
+    public Map<String, String> readPropertiesInMap(String path) {
+        if (path == null) {
             throw new NullPointerException();
         }
-        return null;
+        Map<String, String> currentProps = new HashMap<>();
+        Properties currentProperties = readPropertiesFromFile(path);
+        for (String key : currentProperties.stringPropertyNames()){
+            currentProps.put(key, currentProperties.getProperty(key));
+        }
+        properties = currentProps;
+        return Collections.unmodifiableMap(currentProps);
     }
 
-    public String getProperty(String key){
-        return  null;
+    private Properties readPropertiesFromFile(String path) {
+        Properties newProperties;
+        newProperties = new Properties();
+        File propertyFile = new File(path);
+        if (!propertyFile.isFile()) {
+            throw new NoSuchPropertyFileException("Wrong properties path");
+        }
+        FileInputStream input = null;
+        try {
+            input = new FileInputStream(propertyFile);
+            newProperties.load(input);
+        } catch (FileNotFoundException e) {
+            System.out.println("shouldn't get here");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("problems with loading properties from file");
+        } finally {
+            try {
+                input.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.out.println("FileInputStream can`t be closed");
+            }
+        }
+        return newProperties;
+    }
+
+
+    public String getProperty(String key) {
+        return null;
     }
 }
